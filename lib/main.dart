611e8c3de1078b2486ef27051a623b22cd8c556e
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-06 11:52:37 +0500

    Adding custom widget answer & styling & passing Callback function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/answer.dart';
 import 'package:flutter_complete_guide/question.dart';
 // void main() {
 //   runApp(MyApp());
@@ -34,21 +35,9 @@ class _MyAppState extends State<MyApp> {
       body: Column(
         children: [
           Question(questions[_questionIndex]),
-          ElevatedButton(
-            onPressed: _answerQuestion, 
-          child: Text('Answer 1'),
-          ),
-          ElevatedButton(
-            onPressed: () => print('Answer 2 chosen!'), 
-            child: Text('Answer 2'),
-            ),
-          ElevatedButton(
-            onPressed: () {
-              //....
-              print('Answer 3 chosen!');
-            }, 
-            child: Text('Answer 3'),
-            ),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
         ],
       ),
     ),
